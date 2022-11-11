2967c6745ca287d7fa426baaade59d64b9c83dd8
qwe916 <qwe916@naver.com>
2022-11-11 23:49:47 +0900

    Initial commit

---
+++ src/main/java/com/example/bubbleskhu/BubbleskhuApplication.java
@@ -0,0 +1,13 @@
+package com.example.bubbleskhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleskhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleskhuApplication.class, args);
+	}
+
+}
