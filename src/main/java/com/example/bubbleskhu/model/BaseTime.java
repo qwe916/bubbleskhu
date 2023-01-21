e903662fc61fe562c130b74a710c19a69104ae45
qwe916 <qwe916@naver.com>
2023-01-22 05:12:07 +0900

    feat : BaseTime entity

--- src/main/java/com/example/bubbleskhu/model/BaseTime.java
+++ src/main/java/com/example/bubbleskhu/model/BaseTime.java
@@ -1,4 +1,20 @@
 package com.example.bubbleskhu.model;
 
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTime {
+    @CreatedDate
+    protected LocalDateTime createdAt;
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
 }
