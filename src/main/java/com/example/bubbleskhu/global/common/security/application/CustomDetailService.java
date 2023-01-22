1e23da6dc9bbd00293f38b79d63a5bbe74753d13
qwe916 <qwe916@naver.com>
2023-01-22 23:15:15 +0900

    feat : UserDetailService

--- src/main/java/com/example/bubbleskhu/global/common/security/application/CustomDetailService.java
+++ src/main/java/com/example/bubbleskhu/global/common/security/application/CustomDetailService.java
@@ -1,12 +1,43 @@
 package com.example.bubbleskhu.global.common.security.application;
 
+import com.example.bubbleskhu.global.common.security.role.Role;
+import com.example.bubbleskhu.member.application.UserService;
+import com.example.bubbleskhu.member.domain.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CustomDetailService implements UserDetailsService {
+    private UserService service;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = service.findUser(username).orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        user.setAuthorities(
+                Stream.concat(
+                        getRoles(user.getRoles()).stream(),
+                                getPrivileges(user.getRoles()).stream()
+                ).collect(Collectors.toList())
+        );
+        return user;
+    }
+
+    private List<SimpleGrantedAuthority> getRoles(List<Role> roles) {
+        return roles.stream()
+                .map(Role::getName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    private List<SimpleGrantedAuthority> getPrivileges(List<Role> roles) {
+        return roles.stream()
+                .flatMap(role -> role.getPrivileges().stream())
+                .map(privilege -> new SimpleGrantedAuthority(privilege.getName()))
+                .collect(Collectors.toList());
     }
 }
